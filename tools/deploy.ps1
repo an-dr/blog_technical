97b9205a07613e80c79876d485a70430569189e8
Andrei Gramakov <mail@agramakov.me>
2021-05-07 17:53:11 +0200

    Add deploy action and script

--- tools/deploy.ps1
+++ tools/deploy.ps1
@@ -3,13 +3,23 @@ $ErrorActionPreference = "Stop"
 pushd "$PSScriptRoot/../src"
 
 hugo
+
+"> Creating of the archive"
+rm -r blog.agramakov.me -ErrorAction Ignore
 mv -v public blog.agramakov.me
 tar -a -c -f blog.zip .\blog.agramakov.me\*
 
-ssh agrakwkb@agramakov.me "rm -v ~/blog.zip"
+"> Sending of the blog"
+ssh "[[ -f ~/blog.zip ]] && rm ~/blog.zip"
 scp "blog.zip" "agrakwkb@agramakov.me:~"
+
+"> Sending of the blog"
 ssh agrakwkb@agramakov.me "rm -rfv ~/blog.agramakov.me"
-ssh agrakwkb@agramakov.me "unzip blog.zip"
+ssh agrakwkb@agramakov.me "unzip ~/blog.zip"
+ssh agrakwkb@agramakov.me "rm ~/blog.zip"
+
+"> Clean local temp files"
 rm -r .\blog.agramakov.me
-"[ DONE ]"
+
+"> [ DONE ]"
 popd
